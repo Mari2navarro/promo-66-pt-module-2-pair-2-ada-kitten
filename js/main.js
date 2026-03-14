01723d11991eef361f250eef2f9b14dacc3729b8
Mari <navarro.ortiz.maricarmen@gmail.com>
2026-03-14 13:40:56 +0100

    no funciona la función

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const plus = document.querySelector(".plus")
 const newform = document.querySelector(".js-newform")
 const cancel = document.querySelector(".js-cancel")
 const searchButton = document.querySelector(".js_button-search")
-const searchDesc = document.querySelector(".js_in_seach_desc")
+const searchDesc = document.querySelector(".js_in_search_desc")
 const kittenOne = `<li class="card">
                     <article>
                         <img class="card_img" src="https://api-pw.dev.adalab.es/gato-siames.webp"
@@ -63,20 +63,19 @@ function addRemove() {
         newform.classList.add("collapsed");
     }
 
+
 }
 
-function kittenFilter() {
+function renderKitten() {
+    list.innerHTML = "";
     if (kittenOneDesc.includes(descSearchText)) {
-
+        list.innerHTML += kittenOne;
     }
     if (kittenTwoDesc.includes(descSearchText)) {
-
+        list.innerHTML += kittenTwo;
     }
     if (kittenThreeDesc.includes(descSearchText)) {
-
-    }
-    else {
-
+        list.innerHTML += kittenThree;
     }
 }
 
@@ -101,6 +100,31 @@ cancel.addEventListener("click", (ev) => {
 searchButton.addEventListener("click", (ev) => {
     ev.preventDefault();
     const descSearchText = searchDesc.value;
-    kittenFilter();
+    renderKitten(kittenData);
+    //No nos funciona la función 
 })
 
+//LISTA OBJETOS
+const kittenData = {
+    kittenOne: {
+        imagen: "https://api-pw.dev.adalab.es/gato-siames.webp",
+        nombre: "Anastacio",
+        description: "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.",
+        raza: "Siamés",
+    },
+
+    kittenTwo: {
+        imagen: "https://api-pw.dev.adalab.es/sphynx-gato.webp",
+        nombre: "Fiona",
+        description: "Produce fascinación y curiosidad. Exótico, raro, bello, extraño…hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.",
+        raza: "Sphynx",
+    },
+
+    kittenThree: {
+        imagen: "https://api-pw.dev.adalab.es/maine-coon-cat.webp",
+        nombre: "Cielo",
+        description: "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.",
+        raza: "Maine Coon",
+
+    }
+}
